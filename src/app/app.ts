e09fcbe127a8891a97a916c92b8ac877cbeaabb8
Juan8516 <juan.ind85@gmail.com>
2025-06-10 22:22:10 -0500

    Creacion de componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductoComponent } from "./producto/producto.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.component.html',
+  styleUrl: './app.css',
+  imports: [ProductoComponent]
 })
 export class App {
-  protected title = '03.Angular';
+  protected title = 'Angular';
 }
---
+++ src/app/producto/producto.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'Mi-Producto',
+  templateUrl:'./producto.component.html',
+  styleUrl: './producto.component.css',
+})
+
+export class ProductoComponent {
+
+}
