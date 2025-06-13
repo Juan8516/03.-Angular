c4a1e382f70b531af1c26f5022d631e895afce03
Juan8516 <juan.ind85@gmail.com>
2025-06-12 22:10:13 -0500

    creacion nuevo componente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductoComponent } from "./producto/producto.component";
+import { DescuentosComponent } from './descuentos/descuentos.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.css',
-  imports: [ProductoComponent]
+  imports: [ProductoComponent, DescuentosComponent]
 })
 export class App {
   protected title = 'Angular';
---
+++ src/app/descuentos/descuentos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "Mi-Descuentos",
+  templateUrl: "./descuentos.component.html",
+  styleUrls: ["./descuentos.component.css"],
+})
+
+export class DescuentosComponent {
+
+}
