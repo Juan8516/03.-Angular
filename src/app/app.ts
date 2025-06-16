69166a60858189122bbdb372dbd7e7d60410b95f
Juan8516 <juan.ind85@gmail.com>
2025-06-15 19:49:44 -0500

    se agrega etiqueta usuario

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductoComponent } from "./producto/producto.component";
 import { DescuentosComponent } from './descuentos/descuentos.component';
+import { Usuario } from "./usuario/usuario";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.css',
-  imports: [ProductoComponent, DescuentosComponent]
+  imports: [ProductoComponent, DescuentosComponent, Usuario]
 })
 export class App {
   protected title = 'Angular';
