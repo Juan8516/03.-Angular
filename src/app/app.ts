b607ccd771d38087d8422442d7689768c844b515
Juan8516 <juan.ind85@gmail.com>
2025-06-16 20:54:44 -0500

    se agrega etiqueta ventas

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { ProductoComponent } from "./producto/producto.component";
 import { DescuentosComponent } from './descuentos/descuentos.component';
 import { Usuario } from "./usuario/usuario";
+import { Ventas } from './ventas/ventas';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.css',
-  imports: [ProductoComponent, DescuentosComponent, Usuario]
+  imports: [ProductoComponent, DescuentosComponent, Usuario, Ventas]
 })
 export class App {
   protected title = 'Angular';
