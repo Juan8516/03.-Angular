85749d5934e4f42c8e11edef2007c108c8edc954
Juan8516 <juan.ind85@gmail.com>
2025-06-26 19:37:25 -0500

    estructuras de control bucle for

--- src/app/usuario/usuario.ts
+++ src/app/usuario/usuario.ts
@@ -10,6 +10,28 @@ export class Usuario {
 
   webCliReference = "https://docs.angular.lat/cli"
 
+  lenguajesProgramacion: string[] = [
+    "javascript",
+    "typescript",
+    "Python",
+    "PHP",
+    "Angular",
+  ]
+
+  marcasComputadoras: {id: number, marca: string}[] = [
+    {id: 1, marca: "Apple"},
+    {id: 2, marca: "Lenovo"},
+    {id: 3, marca: "HP"},
+    {id: 4, marca: "Asus"},
+    {id: 5, marca: "Dell"},
+  ]
+
+  marcasMonitores: {id: number, marca: string}[] = [];
+
+  ngOnInit() {
+    this.marcasMonitores = [];
+  }
+
   redirigir = true;
 
   cambioRedireccion() {
