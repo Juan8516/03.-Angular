74f566146f20d71e3312f37c4463d2d359c6acbb
Juan8516 <juan.ind85@gmail.com>
2025-06-27 22:39:18 -0500

    mas sobre plantillas: defer y placeholder

--- src/app/usuario/usuario.ts
+++ src/app/usuario/usuario.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Ventas } from '../ventas/ventas';
+import { ProductoComponent } from '../producto/producto.component';
 
 @Component({
   selector: 'app-usuario',
   standalone: true,
-  imports: [Ventas],
+  imports: [Ventas, ProductoComponent],
   templateUrl: './usuario.html',
   styleUrl: './usuario.css'
 })
