e09c82663ac93363c181605064694c3c4f645d11
Juan8516 <juan.ind85@gmail.com>
2025-07-02 21:27:41 -0500

    uso de directivas: ngClass

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductoComponent } from "./producto/producto.component";
 import { Usuario } from './usuario/usuario';
+import { Ventas } from './ventas/ventas';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.css',
-  imports: [Usuario]
+  imports: [Ventas]
 })
 export class App {
   protected title = 'Angular';
--- src/app/ventas/ventas.ts
+++ src/app/ventas/ventas.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ProductoComponent } from '../producto/producto.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-ventas',
   standalone: true,
-  imports: [],
+  imports: [ProductoComponent, NgClass],
   templateUrl: './ventas.html',
   styleUrl: './ventas.css'
 })
 export class Ventas {
+
+  public colorear: boolean = false;
 
 }
